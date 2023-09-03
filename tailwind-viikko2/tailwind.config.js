7534da2c1e73e4cdfb9d1a1c0504922867e9852c
Ubuntu <hakkerikone-server@hakkerikone-server.x4buanhd0ehe1p4nobuqgqjfof.fx.internal.cloudapp.net>
2023-09-03 13:18:19 +0000

    push new files

---
+++ tailwind-viikko2/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        main: '#050804',
+        secondary: '#19201A',
+        third: '#2C312B',
+        fourth: '#9E8772',
+      },
+    },
+    plugins: [],
+  }
+};
